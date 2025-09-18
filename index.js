d6f25516f31d274e2bf553730dbdce4c27fd4ceb
shivanibhadauria <shivanibhadauria921997@gmail.com>
2025-09-18 12:29:51 +0530

    feat(task-manager): implement task creation, viewing and editing
    - Add POST endpoint for creating tasks with title and details
    - Add GET endpoints for viewing and editing tasks
    - Create new EJS templates for task display and editing
    - Update index view to link to task details and edit pages
    - Store tasks as text files in ./tasks directory

--- index.js
+++ index.js
@@ -9,16 +9,37 @@ app.use(express.static(path.join(__dirname, "public")))
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  fs.readdir("./tasks" , function(err, tasks) {
-    res.render("index", {tasks: tasks});
+  fs.readdir("./tasks", function (err, tasks) {
+    res.render("index", { tasks: tasks });
     console.log(tasks);
   });
 });
 
-app.get("/profile/:name", (req, res) => {
-  
-  res.send(req.params.name)
+app.post("/create", (req, res) => {
+  fs.writeFile(`./tasks/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error creating task');
+    }
+    console.log(req.body);
+    res.redirect('/');
+  });
 });
+
+app.get("/tasks/:filename", (req, res) => {
+  fs.readFile(`./tasks/${req.params.filename}`, "utf-8", function (err, filedata) {
+    res.render('show', { filename: req.params.filename, filedata: filedata });
+    console.log(filedata);
+  })
+}
+)
+
+app.get("/edit/:filename", (req, res) => {
+  res.render('edit', { filename: req.params.filename });
+}
+)
+
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
