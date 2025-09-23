0d47f803175e43cb8ebf06c0e93e03d8ae0053d3
shivanibhadauria <shivanibhadauria921997@gmail.com>
2025-09-23 14:27:30 +0530

    feat: Add post model and user post association

--- mongodb-part-5/app.js
+++ mongodb-part-5/app.js
@@ -13,109 +13,111 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-app.get('/create', (req, res) => {
-    res.render('create');
-});
+// app.get('/', (req, res) => {
+//     res.render('index');
+// });
+// app.get('/login', (req, res) => {
+//     res.render('login');
+// });
+// app.get('/create', (req, res) => {
+//     res.render('create');
+// });
 
 
 
-app.post("/createuser", (req, res) => {
-    let { username, password, email, age } = req.body;
+// app.post("/createuser", (req, res) => {
+//     let { username, password, email, age } = req.body;
 
-    bcrypt.genSalt(10, function (err, salt) {
+//     bcrypt.genSalt(10, function (err, salt) {
 
-        bcrypt.hash(password, salt, async function (err, hash) {
-            let loginuser = await loginusers.create({
-                username,
-                password: hash,
-                email,
-                age
-            })
-            res.send(loginuser)
+//         bcrypt.hash(password, salt, async function (err, hash) {
+//             let loginuser = await loginusers.create({
+//                 username,
+//                 password: hash,
+//                 email,
+//                 age
+//             })
+//             res.send(loginuser)
 
-        });
-    });
+//         });
+//     });
 
-    let token = jwt.sign({ email }, "secret");
-    res.cookie("token", token);
-
-
-
-
-})
-
-
-app.post("/loginuser", async (req, res) => {
-
-    let user = await loginusers.findOne({ username: req.body.username });
-    console.log(req.body)
-    console.log(user);
-    if (!user) {
-        res.send("user not found");
-    }
-
-    bcrypt.compare(req.body.password, user.password, function (err, result) {
-        if (result) {
-            let token = jwt.sign({ email: user.email }, "secret");
-            res.cookie("token", token);
-
-            res.send("yes you can login")
-        } else {
-            res.send("you can not log in")
-        }
+//     let token = jwt.sign({ email }, "secret");
+//     res.cookie("token", token);
 
 
 
-    })
+
+// })
+
+
+// app.post("/loginuser", async (req, res) => {
+
+//     let user = await loginusers.findOne({ username: req.body.username });
+//     console.log(req.body)
+//     console.log(user);
+//     if (!user) {
+//         res.send("user not found");
+//     }
+
+//     bcrypt.compare(req.body.password, user.password, function (err, result) {
+//         if (result) {
+//             let token = jwt.sign({ email: user.email }, "secret");
+//             res.cookie("token", token);
+
+//             res.send("yes you can login")
+//         } else {
+//             res.send("you can not log in")
+//         }
 
 
 
-})
+//     })
 
-app.get("/logout", (req, res) => {
-    res.cookie("token", "");
-    res.redirect("/login");
-})
 
-app.get("/allusers", async (req, res) => {
-    let allusers = await userModel.find();
-    res.render('showUsers', { users: allusers });
-})
 
-app.post("/create", async (req, res) => {
-    const { name, email, image } = req.body;
+// })
 
-    let userModal = await userModel.create({
-        name,
-        email,
-        image
-    })
+// app.get("/logout", (req, res) => {
+//     res.cookie("token", "");
+//     res.redirect("/login");
+// })
 
-    res.redirect("/allusers");
-})
+// app.get("/allusers", async (req, res) => {
+//     let allusers = await userModel.find();
+//     res.render('showUsers', { users: allusers });
+// })
 
-app.post("/delete/:id", async (req, res) => {
-    let user = await userModel.findOneAndDelete({ _id: req.params.id });
-    res.redirect("/allusers");
-})
+// app.post("/create", async (req, res) => {
+//     const { name, email, image } = req.body;
 
-app.get("/update/:id", async (req, res) => {
-    let user = await userModel.findOne({ _id: req.params.id });
-    res.render("edit", { user })
+//     let userModal = await userModel.create({
+//         name,
+//         email,
+//         image
+//     })
 
-})
-app.post("/edit/:id", async (req, res) => {
-    let { name, email, image } = req.body;
+//     res.redirect("/allusers");
+// })
 
-    let user = await userModel.findOneAndUpdate({ _id: req.params.id }, { name, email, image }, { new: true })
-    res.redirect("/allusers")
-})
+// app.post("/delete/:id", async (req, res) => {
+//     let user = await userModel.findOneAndDelete({ _id: req.params.id });
+//     res.redirect("/allusers");
+// })
+
+// app.get("/update/:id", async (req, res) => {
+//     let user = await userModel.findOne({ _id: req.params.id });
+//     res.render("edit", { user })
+
+// })
+// app.post("/edit/:id", async (req, res) => {
+//     let { name, email, image } = req.body;
+
+//     let user = await userModel.findOneAndUpdate({ _id: req.params.id }, { name, email, image }, { new: true })
+//     res.redirect("/allusers")
+// })
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- mongodb-part-5/models/userlogin.js
+++ mongodb-part-5/models/userlogin.js
@@ -8,7 +8,12 @@ const userLoginSchema = mongoose.Schema({
     username: String,
     password: String,
     email: String,
-    age: Number
+    age: Number,
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'post'
+
+    }]
 })
 
 module.exports = mongoose.model('loginusers', userLoginSchema);
